bfe5125d8fd47247d56bcf333f84371ea64b0214
Noe Carl <noe.carldev@gmail.com>
2025-05-22 08:08:45 +0200

    Remove backend training project files including package.json, TypeScript source files, and configuration.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as http from "http";
-import * as t from "./test";
-
-http
-  .createServer(function (req: http.IncomingMessage, res: http.ServerResponse) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(t.test());
-  })
-  .listen(8080);
